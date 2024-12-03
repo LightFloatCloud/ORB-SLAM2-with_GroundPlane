bd17ddc58977b9bd6bf9a5ccca7eca62cea0d248
LightFloatCloud <chenyexin123@foxmail.com>
2024-12-03 15:48:58 +0000

    Rt歧义无法解决，通过约束地面向量完成初始化

--- src/Initializer.cc
+++ src/Initializer.cc
@@ -823,6 +823,11 @@ logfile << "d3 = " << d3 <<";" << std::endl;
     float bestParallax = -1;
     vector<cv::Point3f> bestP3D;
     vector<bool> bestTriangulated;
+    // My revise 添加second优选判断
+    int secondBestSolutionIdx = -1;
+    float secondBestParallax = -1;
+    vector<cv::Point3f> secondBestP3D;
+    vector<bool> secondBestTriangulated;
 
     // Instead of applying the visibility constraints proposed in the Faugeras' paper (which could fail for points seen with low parallax)
     // We reconstruct all hypotheses and check in terms of triangulated points and parallax
@@ -849,6 +854,11 @@ logfile << "];" << std::endl;
         if(nGood>bestGood)
         {
             secondBestGood = bestGood;
+            secondBestSolutionIdx = bestSolutionIdx;
+            secondBestParallax = bestParallax;
+            secondBestP3D = bestP3D;
+            secondBestTriangulated = bestTriangulated;
+            // secondBestGood = bestGood;
             bestGood = nGood;
             bestSolutionIdx = i;
             bestParallax = parallaxi;
@@ -857,13 +867,19 @@ logfile << "];" << std::endl;
         }
         else if(nGood>secondBestGood)
         {
+            // secondBestGood = nGood;
+            // Update second best
             secondBestGood = nGood;
+            secondBestSolutionIdx = i;
+            secondBestParallax = parallaxi;
+            secondBestP3D = vP3Di;
+            secondBestTriangulated = vbTriangulatedi;
         }
     }
 
 
-    if(secondBestGood<mSecGoodFactor*bestGood && bestParallax>=minParallax && bestGood>minTriangulated && bestGood>0.9*N)
-    {
+    if(secondBestGood<mSecGoodFactor*bestGood && vn[bestSolutionIdx].at<float>(1) < 0 && bestParallax>=minParallax && bestGood>minTriangulated && bestGood>0.9*N)
+    {   // 最好允许 mSecGoodFactor 大一些，大于0.7
         vR[bestSolutionIdx].copyTo(R21);
         vt[bestSolutionIdx].copyTo(t21);
 
@@ -873,10 +889,26 @@ logfile << "];" << std::endl;
         vP3D = bestP3D;
         vbTriangulated = bestTriangulated;
 
-        std::cout << "secondBestGood: " 
+        std::cout << "secondBestGood Rate: " 
                 << std::fixed << std::setprecision(3) << static_cast<float>(secondBestGood) / bestGood 
                 << ", bestParallax: " << std::setprecision(2) << bestParallax
                 << ", bestGood: " << bestGood 
+                << std::endl;
+logfile.close();
+        return true;
+    }
+    else if(secondBestSolutionIdx>=0 && vn[secondBestSolutionIdx].at<float>(1) < 0 && secondBestGood > 0.5 * bestGood 
+        && secondBestParallax>=minParallax && secondBestGood>minTriangulated)
+    {
+        vR[secondBestSolutionIdx].copyTo(R21);
+        vt[secondBestSolutionIdx].copyTo(t21);
+        vn[secondBestSolutionIdx].copyTo(n1);
+        vP3D = secondBestP3D;
+        vbTriangulated = secondBestTriangulated;
+        std::cout << "Best Ground is bad, secondBestGood Rate: " 
+                << std::fixed << std::setprecision(3) << static_cast<float>(secondBestGood) / bestGood 
+                << ", secondBestParallax: " << std::setprecision(2) << secondBestParallax
+                << ", secondBestGood: " << secondBestGood 
                 << std::endl;
 logfile.close();
         return true;
@@ -891,7 +923,7 @@ logfile.close();
         if(bestGood <= minTriangulated)
             std::cout << "bestGood is less than the minimum required triangulated points. bestGood: " << bestGood << std::endl;
         if(bestGood <= 0.9*N)
-            std::cout << "bestGood is less than 90%% of the total inlier points." << std::endl;
+            std::cout << "bestGood is less than 90% of the total inlier points." << std::endl;
         
     }
 
